4934f44896969fa16cef3d07b7d21a461eb01cfb
Shanghai Fierce <monitechi8@gmail.com>
2020-08-12 13:17:41 -0500

    testing variables and console log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,34 @@
-window.alert ("This is an alert! Woohoo! JavaScript is running I guess!");
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+var name = "Monica";
+console.log(name);
+
+var playerName = "Tony the Robot";
+
+// Tony the Robot is ready for battle!
+console.log("Tony the Robot" + " is ready for battle!");
+
+// "Tony the Robot is ready for battle!"
+console.log(playerName + " is ready for battle!");
+
+// "Your robot, Tony the Robot, has won!
+console.log("Your robot, " + playerName + ", has won!");
+
+
+
+
+// this creates a function name "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
